bd6a0817c1ee2c50e455094094924a1f2d0bc5dc
Roberto Barreto <robertoboficial@gmail.com>
2024-11-25 16:08:23 -0300

    rebranding

--- automacao/pegar_posicao.py
+++ automacao/pegar_posicao.py
@@ -1,17 +1,5 @@
-<<<<<<< HEAD:automacao/pegar_posicao.py
 import time
 import pyautogui
 
 time.sleep(5)
 print(pyautogui.position())
-
-
-
-# (x=732, y=395)
-=======
-import time
-import pyautogui
-
-time.sleep(5)
-print(pyautogui.position())
->>>>>>> dcebe0dc248836530640ddc96387d66f84f484fa:Aula1/pegar_posicao.py
